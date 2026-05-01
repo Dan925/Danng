03eb06d2338bc44f45b32b80d76166bbc1a8d249
Dan925 <dngen029@uottawa.ca>
2026-04-30 22:45:29 -0400

    Modernize resume and life details

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://danng.netlify.app",
+    output: "static",
+    adapter: netlify(),
     integrations: [sitemap()],
     vite: {
         plugins: [tailwindcss()],
