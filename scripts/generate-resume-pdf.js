2c9f1a73fe155e125365da44aad6d348da1881f7
Dan925 <dngen029@uottawa.ca>
2026-05-04 23:44:35 -0400

    Override netlify build command

--- scripts/generate-resume-pdf.js
+++ scripts/generate-resume-pdf.js
@@ -10,7 +10,6 @@ import puppeteer from 'puppeteer';
     const pdfPath = path.join(distDir, 'resume.pdf');
 
     const browser = await puppeteer.launch({
-        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
         args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
     const page = await browser.newPage();
